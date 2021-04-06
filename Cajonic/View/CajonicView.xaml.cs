81676f684ccf7eea6bff86f8f21feaee1716bd52
Adrien <Dracose12@gmail.com>
2021-04-06 14:54:06 +0100

    Add view folder

--- Cajonic/View/CajonicView.xaml.cs
+++ Cajonic/View/CajonicView.xaml.cs
@@ -18,9 +18,9 @@ namespace Cajonic
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CajonicView : Window
     {
-        public MainWindow()
+        public CajonicView()
         {
             InitializeComponent();
         }
