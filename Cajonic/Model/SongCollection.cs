9de8f4f040b5e79cde954005e1d561afa786f6f0
Adrien <Dracose12@gmail.com>
2021-04-07 13:35:32 +0100

    Add SongCollection

---
+++ Cajonic/Model/SongCollection.cs
@@ -0,0 +1,34 @@
+﻿using Cajonic.Services;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Cajonic.Model
+{
+    public class SongCollection
+    {
+        private readonly ISongLoader _loader;
+
+        public ObservableCollection<Song> SongList { get; }
+
+        public SongCollection(ISongLoader ql)
+        {
+            SongList = new ObservableCollection<Song>();
+            _loader = ql;
+        }
+
+        public void Load(string filepath)
+        {
+            var songs = _loader.Load(filepath);
+
+            foreach (Song s in songs)
+            {
+                SongList.Add(s);
+            }
+        }
+
+        public double TotalSeconds()
+        {
+            return SongList.Sum(s => s.Duration.TotalSeconds);
+        }
+    }
+}
