a5a697d9b57427bda95b0eda58359b2cb5c838c3
Adrien <Dracose12@gmail.com>
2021-04-07 09:44:55 +0100

    Add song model and interface for Loader

---
+++ Cajonic/Model/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace Cajonic.Model
+{
+    public class Song
+    {
+        public string Artist { get; set; }
+        public string AlbumArtist { get; set; }
+        public string Album { get; set; }
+        public string Composer { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public int PlayCount { get; set; }
+        public string Comments { get; set; }
+        public string Lyrics { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string DisplayDuration
+        {
+            get
+            {
+                return Duration.ToString("mm\\:ss");
+            }
+        }
+        public BitmapImage Artwork { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ Cajonic/Services/IQueueLoader.cs
@@ -0,0 +1,13 @@
+﻿using Cajonic.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cajonic.Services
+{
+    public interface IQueueLoader
+    {
+        // This is where you get the metadata you want, and potentially make this function parallelized
+        List<Song> Load(string path);
+    }
+}
